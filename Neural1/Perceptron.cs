041a7be1806bb65943385de786208291d6cfe0e4
IvanKuzminyh <ivan.kuzminyh2019@gmail.com>
2023-04-14 00:04:15 +0500

    Добавьте файлы проекта.

---
+++ Neural1/Perceptron.cs
@@ -0,0 +1,35 @@
+﻿namespace Neural1
+{
+    internal class Perceptron
+    {
+        public Perceptron() 
+        {
+            this.Weights = new List<double>();
+            Random random = new Random();
+            // 2 веса под входы, один под смещение
+            for(int i = 0; i < 3; i++)
+            {
+                this.Weights.Add(random.NextDouble());
+            }
+        }
+        public List<double> Weights { get; set; }
+        public int Solve(List<int> x)
+        {
+            // Во входной вектор добавляется постоянный элемент для веса смещения
+            x.Add(1);
+            double sum = 0;
+            for (int i = 0; i < x.Count; ++i)
+            {
+                sum += x[i] * Weights[i];
+            }
+            // Удаляется постоянный элемент
+            x.RemoveAt(2);
+            return ActivationFunction(sum);
+        }
+        int ActivationFunction(double x)
+        {
+            if (x >= 0.5) return 1;
+            else return 0;
+        }
+    }
+}
---
+++ Neural1/Program.cs
@@ -0,0 +1,48 @@
+﻿using Neural1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Dictionary<List<int>, int> examples = new Dictionary<List<int>, int>();
+        examples.Add(new List<int> { 0, 0 }, 1);
+        examples.Add(new List<int> { 0, 1 }, 0);
+        examples.Add(new List<int> { 1, 0 }, 1);
+        examples.Add(new List<int> { 1, 1 }, 1);
+        Perceptron perceptron = new Perceptron();
+        int genCount = 0;
+        int successCount = 0;
+        do
+        {
+            successCount = 0;
+            Console.WriteLine($"GENERATION #{genCount}");
+            foreach (var example in examples)
+            {
+                int answer = perceptron.Solve(example.Key);
+                Console.WriteLine($"f({example.Key[0]}, {example.Key[1]}) = {example.Value}, answer = {answer}");
+
+                if (answer == example.Value)
+                {
+                    successCount++;
+                }
+                else
+                {
+                    int weightCount = perceptron.Weights.Count;
+                    for (int i = 0; i < weightCount - 1; i++)
+                    {
+                        perceptron.Weights[i] += 0.1 * (example.Value - answer) * example.Key[i];
+                    }
+                    // для исправления берется постоянный вход смещения
+                    perceptron.Weights[weightCount - 1] += 0.1 * (example.Value - answer) * perceptron.Weights[weightCount - 1];
+
+                }
+            }
+            genCount++;
+        } while (successCount < examples.Count);
+        Console.WriteLine($"{genCount} generations passed");
+        foreach(var example in examples)
+        {
+            Console.WriteLine($"f({example.Key[0]}, {example.Key[1]}) = {perceptron.Solve(example.Key)}");
+        }
+    }
+}
